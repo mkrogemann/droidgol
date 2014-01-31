84a8f1b3b145f1767e22c819a56f0759984cf250
Markus Krogemann <markus@krogemann.de>
2014-01-31 17:45:11 +0100

    nice to know how many rows/columns my Nexus 5 will do, but how to calculate these on other devices?

--- src/org/rubyspa/droidgol/Game.java
+++ src/org/rubyspa/droidgol/Game.java
@@ -2,14 +2,40 @@ package org.rubyspa.droidgol;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Pair;
+import android.widget.TextView;
 
 public class Game extends Activity
 {
+
+    private TextView boardView;
+    private Pair<Integer, Integer> boardDimensions;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initialize();
     }
+
+    private void initialize() {
+        this.boardView = (TextView) findViewById(R.id.gameBoard);
+        this.boardDimensions = computeDimensions();
+        this.boardView.setText(String.format(
+                "\n\n\n\n\n\n                " +
+                "\n     Game will extend over" +
+                "\n     %d columns and" +
+                "\n     %d rows" +
+                "\n     (touch to start)", boardDimensions.first, boardDimensions.second));
+    }
+
+    private Pair<Integer, Integer> computeDimensions() {
+        // TODO: How do you reliably find out how many 'o's per line and how many lines we can fit on the screen?
+        Integer width = 38;
+        Integer height = 28;
+        return Pair.create(width, height);
+    }
+
 }
